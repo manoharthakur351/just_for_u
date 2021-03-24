66441fc3033f6d56cd28f75c0cde0f6952bf240a
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-03-24 23:26:33 +0530

    Create triangles.py

---
+++ Python/Basics/patterns/triangles.py
@@ -0,0 +1,137 @@
+"""
+
+program to print
+
+*
+
+* *
+
+* * *
+
+* * * *
+
+* * * * *
+
+* * * * * *
+
+* * * * * * *
+
+* * * * * * * *
+
+* * * * * * * * *
+
+"""
+
+for a in range(0,10):
+
+	for b in range (0,a):		print('*',end=' ')
+
+	print()
+
+"""
+
+program to print
+
+* * * * * * * * * *
+
+* * * * * * * * *
+
+* * * * * * * *
+
+* * * * * * *
+
+* * * * * *
+
+* * * * *
+
+* * * *
+
+* * *
+
+* *
+
+*
+
+"""
+
+for a in range(10,0,-1):
+
+	for b in range (0,a):
+
+		print('*',end=' ')
+
+	print()
+
+"""
+
+program to print
+
+* * * * * * * * * *
+
+  * * * * * * * * *
+
+    * * * * * * * *
+
+      * * * * * * *
+
+        * * * * * *
+
+          * * * * *
+
+            * * * *
+
+              * * *
+
+                * *
+
+                  *
+
+"""
+
+for a in range(0,10):
+
+	for b in range (0,a):
+
+		print(' ',end=' ')
+
+	for b in range(0,10-a):
+
+		print('*',end = ' ' )
+
+	print()
+
+"""
+
+program to print
+
+                  *
+
+                * *
+
+              * * *
+
+            * * * *
+
+          * * * * *
+
+        * * * * * *
+
+      * * * * * * *
+
+    * * * * * * * *
+
+  * * * * * * * * *
+
+"""
+
+for a in range(0,10):
+
+	for b in range (0,10-a):
+
+		print(' ',end=' ')
+
+	for b in range(0,a):
+
+		print('*',end = ' ' )
+
+	print()
