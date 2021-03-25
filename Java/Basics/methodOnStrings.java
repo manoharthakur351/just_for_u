6659c560a39afc0f91aaa36339fa3ba21259e27d
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-03-26 01:33:29 +0530

    Create methodOnStrings.java

---
+++ Java/Basics/methodOnStrings.java
@@ -0,0 +1,62 @@
+/*
+In this file here are some examples of methods that we can
+apply on a string.
+
+you can un comment paired codes to experience every method
+individually
+*/
+
+package com.manicrover;
+
+public class StringMethod {
+
+  public static void main(String[] args) {
+    System.out.println();
+    String name = new String("  Manohar Lal Thahur. ");
+
+    System.out.println("original :");
+    System.out.println(name);
+    System.out.println();
+    System.out.println("result :");
+
+    // int len = name.length();
+    // System.out.println(len);
+
+    // String lower = name.toLowerCase();
+    // System.out.println(lower);
+
+    // String upper = name.toUpperCase();
+    // System.out.println(upper);
+
+    // String sub = name.substring(4,9);
+    // System.out.println(sub);
+
+    // String replacedchar = name.replace("a","ooo");
+    // System.out.println(replacedchar);
+
+    // String replacedstr = name.replace('a','_');
+    // System.out.println(replacedstr);
+
+    // boolean startedt = name.startsWith(" ");
+    // System.out.println(startedt);
+
+    // boolean startedf = name.startsWith("a");
+    // System.out.println(startedf);
+
+    // boolean ends = name.startsWith(" ");
+    // System.out.println(ends);
+
+    // boolean endf = name.startsWith("q");
+    // System.out.println(endf);
+
+    // char position = name.charAt(10);
+    // System.out.println(position);
+
+    // int index = name.indexOf(" Lal");
+    // System.out.println(index);
+
+    // String trimmed = name.trim();
+    // System.out.println(trimmed);
+
+  }
+}
