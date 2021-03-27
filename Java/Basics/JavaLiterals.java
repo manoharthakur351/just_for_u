a47dd1fd454081f743e01f1b76550b49f228c34a
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-03-27 16:42:09 +0530

    Create JavaLiterals.java

---
+++ Java/Basics/JavaLiterals.java
@@ -0,0 +1,23 @@
+package com.manicrover;
+
+public class Literals {
+
+	public static void main(String[] args) {
+	byte age = 100;
+	char character = 'A'; // char uses ''
+	float fi = 1.1f; // default value d hoti he .•. floating point number k liye f likhana hota hai 
+	double di = 1.3d; // D or d means double is the default mode of decimals.
+	boolean bi = true; // isme t small hota he.
+	long age_dino = 10007787678l; // by default ye 1 int he to mujhe l ye L lagana Ho Ga.
+	short value = 10; // s wagera ni likhna
+	int integer = 100; // i wagera ni likhna
+	String str = "string"; // string use ""
+	System.out.println(str);
+	System.out.println(character);
+	System.out.println(fi);
+	System.out.println(di);
+	System.out.println(bi);
+	System.out.println(di);
+	}
+
+}
