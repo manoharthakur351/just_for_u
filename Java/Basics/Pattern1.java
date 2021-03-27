ffc09c739ab4e07d766cc6b9495ecfc48e1fa464
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-03-28 00:49:41 +0530

    Create Pattern1.java

---
+++ Java/Basics/Pattern1.java
@@ -0,0 +1,14 @@
+package com.manicrover;
+
+public class Tri1 {
+
+  public static void main(String[] args) {
+
+    for (int n = 0; n < 5; n++) {
+      for (int m = n; m< 5 ; m++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
