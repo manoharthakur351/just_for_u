6e71f21fbad6a3e239e55717a0a44594acc896a4
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-03-27 16:38:50 +0530

    Create IfElse.java

---
+++ Java/Basics/IfElse.java
@@ -0,0 +1,13 @@
+package com.manicrover;
+
+public class IfElse {
+
+  public static void main(String[] args) {
+
+    if (2 == 25) {
+      System.out.println("yo");
+    } else {
+      System.out.println("no");
+    }
+  }
+}
