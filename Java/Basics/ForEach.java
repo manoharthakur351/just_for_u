d715068f9ee526f5f87e960c41d055520150c6f8
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-03-28 18:37:33 +0530

    Create ForEach.java

---
+++ Java/Basics/ForEach.java
@@ -0,0 +1,12 @@
+package manicrover;
+
+public class ForEach {
+
+  public static void main(String[] args) {
+    int[] list = {1, 23, 4, 6, 2, 4, 33};
+
+    for (int element : list) {
+      System.out.println(element);
+    }
+  }
+}
