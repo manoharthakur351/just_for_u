8d23a188a17f5f9fd24311562ba21aa434edbf46
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-03-28 12:17:59 +0530

    Create BitwiseOperators.java

---
+++ Java/Basics/BitwiseOperators.java
@@ -0,0 +1,27 @@
+package manicrover;
+
+public class Bitwise {
+
+  public static void main(String[] args) {
+    System.out.print("true and false : ");
+    System.out.println(true && false);
+
+    System.out.print("true and true : ");
+    System.out.println(true && true);
+
+    System.out.print("false and false : ");
+    System.out.println(false && false);
+
+    System.out.print("true or false : ");
+    System.out.println(true || false);
+
+    System.out.print("true or true : ");
+    System.out.println(true || true);
+
+    System.out.print("false or false : ");
+    System.out.println(false || false);
+
+    System.out.print("not true : ");
+    System.out.println(!true);
+  }
+}
