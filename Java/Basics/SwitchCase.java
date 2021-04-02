50577a9a0603bc7c95e299d75180a223fdd2888e
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-04-03 04:23:01 +0530

    Update SwitchCase.java

--- Java/Basics/SwitchCase.java
+++ Java/Basics/SwitchCase.java
@@ -1,29 +1,95 @@
-package com.manicrover;
+package manicrover;
 
 import java.util.Scanner;
 
 public class SwitchCase {
 
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int a;
+
     while (true) {
-
-      a = input.nextInt();
-
+      Scanner sc = new Scanner(System.in);
+      String a = sc.next();
       switch (a) {
-        case 100:
-          System.out.println("hundred");
+        case "a":
+          System.out.println("apple");
           break;
-        case 200:
-          System.out.println("two hundred");
+        case "b":
+          System.out.println("ball");
           break;
-        case 300:
-          System.out.println("three hundred");
+        case "c":
+          System.out.println("cat");
           break;
-        default:
-        System.out.println("no value");
+        case "d":
+          System.out.println("dog");
+          break;
+        case "e":
+          System.out.println("elephant");
+          break;
+        case "f":
+          System.out.println("football");
+          break;
+        case "g":
+          System.out.println("god");
+          break;
+        case "h":
+          System.out.println("hat");
+          break;
+        case "i":
+          System.out.println("increadible");
+          break;
+        case "j":
+          System.out.println("joy");
+          break;
+        case "k":
+          System.out.println("killer kind");
+          break;
+        case "l":
+          System.out.println("light");
+          break;
+        case "m":
+          System.out.println("master");
+          break;
+        case "n":
+          System.out.println("nationa");
+          break;
+        case "o":
+          System.out.println("outstanding");
+          break;
+        case "p":
+          System.out.println("pro");
+          break;
+        case "q":
+          System.out.println("questions");
+          break;
+        case "r":
+          System.out.println("range");
+          break;
+        case "s":
+          System.out.println("system");
+          break;
+        case "t":
+          System.out.println("temper");
+          break;
+        case "u":
+          System.out.println("unity");
+          break;
+        case "v":
+          System.out.println("virgin");
+          break;
+        case "w":
+          System.out.println("wired");
+          break;
+        case "x":
+          System.out.println("x-man");
+          break;
+        case "y":
+          System.out.println("yet");
+          break;
+        case "z":
+          System.out.println("zone");
+          break;
       }
+      sc.close();
     }
   }
 }
