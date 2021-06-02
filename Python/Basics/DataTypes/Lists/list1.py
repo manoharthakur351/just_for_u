4083d6657b27b97a7a25f3a2007c46273fb01f0c
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-06-02 15:50:45 +0530

    Update list1.py
    Added CONCEPT OF slicing and indexing

--- Python/Basics/DataTypes/Lists/list1.py
+++ Python/Basics/DataTypes/Lists/list1.py
@@ -1,2 +1,21 @@
-#empty list
-Lst = []
+# creating a list
+lst = ['q','b','c','d','e','f','g','h','i','j','k']
+print(lst)
+
+# magic of "*"
+print(*lst)
+
+# indexing
+a = lst[1]
+print(a)
+
+b = lst[-1]
+print(b)
+
+# slicing
+c = lst[0:4]
+print(c)
+
+# step in slicing
+d = lst[3:9:3]
+print(d)
