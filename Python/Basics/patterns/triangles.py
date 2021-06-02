138f7c679b64d36ff4f68456dc880465ad084843
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2021-06-02 15:40:42 +0530

    Update triangles.py

--- Python/Basics/patterns/triangles.py
+++ Python/Basics/patterns/triangles.py
@@ -1,137 +1,71 @@
 """
-
 program to print
 
 *
 
 * *
-
 * * *
-
 * * * *
-
 * * * * *
-
 * * * * * *
-
 * * * * * * *
-
 * * * * * * * *
-
 * * * * * * * * *
-
 """
-
 for a in range(0,10):
-
 	for b in range (0,a):		print('*',end=' ')
-
 	print()
-
 """
-
 program to print
-
 * * * * * * * * * *
-
 * * * * * * * * *
-
 * * * * * * * *
-
 * * * * * * *
-
 * * * * * *
-
 * * * * *
-
 * * * *
-
 * * *
-
 * *
-
 *
-
 """
-
 for a in range(10,0,-1):
-
 	for b in range (0,a):
-
 		print('*',end=' ')
-
 	print()
-
 """
-
 program to print
-
 * * * * * * * * * *
-
   * * * * * * * * *
-
     * * * * * * * *
-
       * * * * * * *
-
         * * * * * *
-
           * * * * *
-
             * * * *
-
               * * *
-
                 * *
-
                   *
-
 """
-
 for a in range(0,10):
-
 	for b in range (0,a):
-
 		print(' ',end=' ')
-
 	for b in range(0,10-a):
-
 		print('*',end = ' ' )
-
 	print()
-
 """
-
-program to print
-
+prgram to print
                   *
-
                 * *
-
               * * *
-
             * * * *
-
           * * * * *
-
         * * * * * *
-
       * * * * * * *
-
     * * * * * * * *
-
   * * * * * * * * *
-
 """
-
 for a in range(0,10):
-
 	for b in range (0,10-a):
-
 		print(' ',end=' ')
-
 	for b in range(0,a):
-
 		print('*',end = ' ' )
-
 	print()
