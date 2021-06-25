edf315f36e65a2c4c2314d20b0874ec34f77b128
Manohar <79231213+manoharthakur-oss@users.noreply.github.com>
2021-06-25 18:02:44 +0530

    Create design1.py

---
+++ Python/Turtle/design1.py
@@ -0,0 +1,18 @@
+import turtle
+
+t = turtle
+t.color('cyan')
+t.bgcolor('black')
+
+
+t.pu()
+t.fd(200)
+t.rt(90)
+t.down()
+t.speed(100)
+
+while True:
+	t.circle(100)
+	t.rt(5)
+	t.fd(10)
+	t.down()
