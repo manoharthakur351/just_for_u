375d26744adff60245c3ff38c372ed8823cd8f5d
Manohar Thakur <79008294+manoharthakur351@users.noreply.github.com>
2023-09-19 11:34:48 +0530

    Update Si.c

--- Clang/Basics/Si.c
+++ Clang/Basics/Si.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    float principal, rate, time, simpleInterest;
+
+    // Input principal amount
+    printf("Enter the principal amount: ");
+    scanf("%f", &principal);
+
+    // Input rate of interest
+    printf("Enter the rate of interest (in percentage): ");
+    scanf("%f", &rate);
+
+    // Input time period in years
+    printf("Enter the time period (in years): ");
+    scanf("%f", &time);
+
+    // Calculate simple interest
+    simpleInterest = (principal * rate * time) / 100;
+
+    // Display the result
+    printf("Simple Interest = %.2f\n", simpleInterest);
+
+    return 0;
+}
+
+https://pastebin.com/k4msDfDU
